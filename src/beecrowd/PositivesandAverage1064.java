5f922a65d5d699686ba8de3d9f1348ba592ba2bf
rafa <ralfrr55@gmail.com>
2022-07-25 18:02:16 -0300

    159 beecrowd

--- src/beecrowd/PositivesandAverage1064.java
+++ src/beecrowd/PositivesandAverage1064.java
@@ -1,9 +1,20 @@
+package beecrowd;
 
 import java.util.Scanner;
 
-public class Traing {
+public class PositivesandAverage1064 {
 
-
+    /**Read 6 values that can be floating point numbers.
+     *  After, print how many of them were positive. In the next line, print the average of all positive
+     *  values typed, with one digit after the decimal point.
+     Input
+     The input consist in 6 numbers that can be integer or floating point values. At least one number will be
+     positive.
+     Output
+     The first output value is the amount of positive numbers. The next line should show the average ofthe
+     positive values ​typed.
+     *
+     */
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -23,10 +34,7 @@ public class Traing {
         }
         resul = soma / 2;
 
-
         System.out.println(p + " valores positivos");
         System.out.println(String.format("%.1f", soma));
     }
 }
-
-
